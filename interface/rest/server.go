f3e43989ecac2d77c7bed2341a6e26556a691e19
Yusaku Matsuki <ymatzki@gmail.com>
2022-05-13 20:46:27 +0900

    feat: add handler

--- interface/rest/server.go
+++ interface/rest/server.go
@@ -4,11 +4,13 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/ymatzki/go-clean-architecture/interface/rest/handler"
 )
 
 func Execute() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		io.WriteString(w, handler.Hello())
 	})
 	log.Printf("Start http server...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
